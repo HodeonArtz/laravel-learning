3efb8ac1d306324e8a6c95b309a792aba9ef628b
HodeonArtz <martidicianolold@gmail.com>
2025-01-29 19:37:33 +0100

    feat: create middleware for validating URLs

---
+++ app/Http/Middleware/ValidateUrl.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ValidateUrl
+{
+  function handle(Request $request, Closure $next)
+  {
+    $urlRegEx =
+      "/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/";
+    $imgUrl = $request->route("film-img-url");
+    if (!preg_match($urlRegEx, $imgUrl)) {
+      redirect("/");
+    }
+    return $next($request);
+  }
+}
