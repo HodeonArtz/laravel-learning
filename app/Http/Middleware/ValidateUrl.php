ba712180e003bba258af95eb01a2a7e50ae87d4a
HodeonArtz <martidicianolold@gmail.com>
2025-02-09 18:33:37 +0100

    refactor: use img_url name input property instead of using input method

--- app/Http/Middleware/ValidateUrl.php
+++ app/Http/Middleware/ValidateUrl.php
@@ -12,7 +12,7 @@ class ValidateUrl
   {
     $urlRegEx =
       '/[-a-zA-Z0-9@:%_\+.~#?&=\/]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&=\/]*)?/';
-    $imgUrl = $request->input("img_url");
+    $imgUrl = $request->img_url;
     if (!preg_match($urlRegEx, $imgUrl)) {
       return redirect("/")->with("error", "La URL es inválida.");
     }
