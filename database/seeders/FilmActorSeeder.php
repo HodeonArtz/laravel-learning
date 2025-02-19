18cc919d26b1280f2ab4c54ed71118e9d8341212
HodeonArtz <martidicianolold@gmail.com>
2025-02-19 16:54:49 +0100

    feat: create seeder for film_actor relation

---
+++ database/seeders/FilmActorSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FilmActorSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $filmIds = DB::table("films")->pluck("id")->toArray();
+    $actorIds = DB::table("actors")->pluck("id")->toArray();
+    foreach ($filmIds as $filmId) {
+      $randomAmountOfRelations = rand(1, 3);
+
+      // Array of random actorIds with random length between 1-3
+      $randomActors = array_map(
+        fn($key) => $actorIds[$key],
+        (array) array_rand($actorIds, $randomAmountOfRelations)
+      );
+
+      foreach ($randomActors as $actorId) {
+        DB::table("films_actors")->insert([
+          "film_id" => $filmId,
+          "actor_id" => $actorId,
+        ]);
+      }
+    }
+  }
+}
