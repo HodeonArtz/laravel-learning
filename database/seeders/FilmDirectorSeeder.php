abe698e4b64ac2de49d78b2334c759a6bb099f52
HodeonArtz <martidicianolold@gmail.com>
2025-02-25 18:33:27 +0100

    #6 creates seeder for relation between films and directors

---
+++ database/seeders/FilmDirectorSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FilmDirectorSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $filmIds = DB::table("films")->pluck("id")->toArray();
+    $directorIds = DB::table("directors")->pluck("id")->toArray();
+    foreach ($filmIds as $filmId) {
+      $randomAmountOfRelations = rand(1, 3);
+
+      // Array of random directorIds with random length between 1-3
+      $randomDirectors = array_map(
+        fn($key) => $directorIds[$key],
+        (array) array_rand($directorIds, $randomAmountOfRelations)
+      );
+
+      foreach ($randomDirectors as $directorId) {
+        DB::table("films_directors")->insert([
+          "film_id" => $filmId,
+          "director_id" => $directorId,
+        ]);
+      }
+    }
+  }
+}
