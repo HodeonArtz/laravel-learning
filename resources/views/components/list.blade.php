77fc2e5f685224107a5ad435d77696c3f50c912b
HodeonArtz <martidicianolold@gmail.com>
2025-03-14 18:17:59 +0100

    #13 formats blade file

--- resources/views/components/list.blade.php
+++ resources/views/components/list.blade.php
@@ -1,43 +1,40 @@
 @extends("layout.master")
 @section("title", $title)
 @section("content")
-<h1>{{$title}}</h1>
+  <h1>{{$title}}</h1>
 
-@if(empty($elements))
+  @if(empty($elements))
     <FONT COLOR="red">No se ha encontrado nada.</FONT>
-@else
+  @else
     <div class="d-flex justify-content-center my-5">
-    <table class="table">
-      <thead>
-
-        <tr> 
-          @foreach($elements as $element)
-          @foreach(array_keys($element) as $key)
-          <th>{{$key}}</th>
-          @endforeach
-          @break
-          @endforeach
-        </tr>
-        
-      </thead>
-
-      <tbody>
-
-        @foreach($elements as $element)
-            <tr>
-              @foreach ($element as $columnName => $data)
-                <td>
-                  @if (str_contains($columnName, "img"))
-                  <img src={{$data}} style="width: 100px; heigth: 120px;" />
-                  @else
-                    {{ $data }} 
-                  @endif
-                </td>
+      <table class="table">
+        <thead>
+          <tr> 
+            @foreach($elements as $element)
+              @foreach(array_keys($element) as $key)
+                <th>{{$key}}</th>
               @endforeach
-            </tr>
-        @endforeach
-      </tbody>
-    </table>
-</div>
-@endif
+              @break
+            @endforeach
+          </tr>
+        </thead>
+
+        <tbody>
+          @foreach($elements as $element)
+              <tr>
+                @foreach ($element as $columnName => $data)
+                  <td>
+                    @if (str_contains($columnName, "img"))
+                      <img src={{$data}} style="width: 100px; heigth: 120px;" />
+                    @else
+                      {{ $data }} 
+                    @endif
+                  </td>
+                @endforeach
+              </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+  @endif
 @endsection
