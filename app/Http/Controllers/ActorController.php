41a0b862af0aab77def31479b01245c34224dc9d
Glen Marti <mdiciano@snapp-solutions.com>
2025-03-21 14:05:54 +0100

    closes #14 creates method for getting actors by decade

--- app/Http/Controllers/ActorController.php
+++ app/Http/Controllers/ActorController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class ActorController extends Controller
 {
-  public function listAllActors()
+  public function listActors()
   {
     // use json_encode and json_decode to transform object to associative array
     $actors = json_decode(json_encode(DB::table("actors")->get()->toArray()), true);
 
     return view("components.list", ["elements" => $actors, "title" => "Listado de todos los actores"]);
+  }
+  public function listActorsByDecade(int $decade)
+  {
+    $decadeStart = Carbon::create($decade, 1, 1)->startOfYear();
+    $decadeEnd = Carbon::create($decade + 9, 12, 31)->endOfYear();
+
+    $actors = json_decode(json_encode(DB::table("actors")->whereBetween("birthdate", [$decadeStart, $decadeEnd])->get()->toArray()), true);
+    return view("components.list", ["elements" => $actors, "title" => "Listado de actores filtrado por década"]);
   }
   public function countActors()
   {
