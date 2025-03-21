85b33111208ef6ee9c1bb92690412704856bce5e
HodeonArtz <martidicianolold@gmail.com>
2025-03-21 18:14:48 +0100

    #15 creates function destroyActor() for deleting actor and return JSON

--- app/Http/Controllers/ActorController.php
+++ app/Http/Controllers/ActorController.php
@@ -21,11 +21,20 @@ class ActorController extends Controller
     $decadeEnd = Carbon::create($decade + 9, 12, 31)->endOfYear();
 
     $actors = json_decode(json_encode(DB::table("actors")->whereBetween("birthdate", [$decadeStart, $decadeEnd])->get()->toArray()), true);
-    return view("components.list", ["elements" => $actors, "title" => "Listado de actores filtrado por década"]);
+    return view("components.list", ["elements" => $actors, "title" => "Listado de actores nacidos en la década de los " . $decade . "'s"]);
   }
   public function countActors()
   {
     $actorCount = DB::table("actors")->count();
     return view("components.message", ["title" => "Actores", "message" => "Actualmente hay $actorCount actor(es)/actriz(ces)"]);
+  }
+  public function destroyActor(int $id)
+  {
+    $affectedRows = DB::table("actors")->delete($id);
+
+    return json_encode([
+      "action" => "delete",
+      "status" => $affectedRows > 0
+    ]);
   }
 }
