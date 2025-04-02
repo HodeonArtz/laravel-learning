4fd2d9b8e96a3cba25fb3b4678d1d07db0d90950
HodeonArtz <martidicianolold@gmail.com>
2025-04-02 19:01:49 +0200

    #28 defines mock data for film factory

--- database/factories/FilmFactory.php
+++ database/factories/FilmFactory.php
@@ -9,15 +9,21 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class FilmFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    $faker = Factory::create();
+    return [
+      "name" => $faker->words(3, true),
+      "year" => $faker->numberBetween(1900, 2024),
+      "genre" => $faker->words(1, true),
+      "country" => $faker->countryISOAlpha3(),
+      "duration" => $faker->numberBetween(90, 320),
+      "img_url" => $faker->imageUrl(),
+    ];
+  }
 }
