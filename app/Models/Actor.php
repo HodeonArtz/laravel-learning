a8cd8dea61a9e36471a0046a16ad64ad67e0f6f6
Glen Marti <martidicianolold@gmail.com>
2025-04-04 18:17:08 +0200

    #29 creates model for Actors

--- app/Models/Actor.php
+++ app/Models/Actor.php
@@ -17,9 +17,23 @@ class Actor extends Model
   protected $table = 'actors';
 
   /**
+   * The primary key for the model.
+   *
+   * @var string
+   */
+  protected $primaryKey = 'id';
+
+  /**
    * Indicates if the IDs are auto-incrementing.
    *
    * @var bool
    */
   public $incrementing = true;
+
+  /**
+   * Indicates if the model should be timestamped.
+   *
+   * @var bool
+   */
+  public $timestamps = true;
 }
